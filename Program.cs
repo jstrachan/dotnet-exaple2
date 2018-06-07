b1d3d5c3161e8df97e6b373fa5ab359472998576
James Strachan <james.strachan@gmail.com>
2018-06-07 10:30:26 +0100

    Initial import

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace dotnet_example
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseConfiguration(config)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseUrls("http://0.0.0.0:5000")
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
